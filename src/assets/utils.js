e2ef889ac58f730f85b9592e2b33d20d4318b0f6
RebelNightmare_ <119054272+Rebel-Nightmare@users.noreply.github.com>
2024-07-01 12:39:13 +0200

    backgrounds change every time you open the window + automatic dark mode

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -18,6 +18,47 @@ function updateDateTime() {
     timeElement.innerHTML = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 }
 
+function switchToDarkMode(is_bottom_dark){
+    div = document.getElementById("main");
+    div.style.color = '#0f0f0f';
+    links = document.getElementsByClassName("link");
+
+    for(let i = 0; i < links.length; i++){
+        links[i].style.color = '#0f0f0f';
+    }
+
+    if(is_bottom_dark){     // if light pages have a dark bottom section
+        document.getElementById("hostname").style.color = '#f5deb3'
+    } else {
+        document.getElementById("hostname").style.color = '#0f0f0f'
+    }
+}
+
+function setBackground(){
+    const num_pngs = 12, num_jpgs = 14;     // set with personal number of pngs and jpgs
+    let randomIndex = 0, image_path, dark_mode_indexes;
+    const is_png = Math.floor(Math.random() * 2);   // 1 means png
+    if(is_png){
+        randomIndex = (Math.floor(Math.random() * num_pngs))+1;  // getting random index for image
+        image_path = `${randomIndex}.png`
+        dark_mode_indexes = [1,2,3]      // pngs that need dark mode
+        if(dark_mode_indexes.includes(randomIndex)){
+            switchToDarkMode(randomIndex == 9 ? true : false);  // example of png that has darker bottom section
+        }
+    } else {
+        randomIndex = (Math.floor(Math.random() * num_jpgs))+1;
+        image_path = `${randomIndex}.jpg`
+        dark_mode_indexes = [1,2,3]      // jpgs that need dark mode
+        if(dark_mode_indexes.includes(randomIndex)){
+            switchToDarkMode(false);
+        }
+    }
+    
+    div = document.getElementById("main");
+    div.style.backgroundImage = `url(assets/images/${image_path})`
+}
+
+
 setInterval(updateDateTime, 1000); // Update every second
 
 // Initial call to display the time immediately on load
@@ -30,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (event.key === 'Enter') {
             const query = consoleInput.value.trim();
             if (query) {
-                const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+                const searchUrl = `https://duckduckgo.com/?q=${encodeURIComponent(query)}`;
                 window.open(searchUrl, '_blank');
                 consoleInput.value = '';
             }
@@ -63,7 +104,7 @@ fetch("favourites.json")
        
         services.forEach(ser => {
             const serviceElement = serviceTemplate.cloneNode(true);
-            serviceElement.getElementsByClassName('serviceName')[0].innerHTML = `<i class="fab ${ser.icon}"></i> ${ser.name}`;
+            serviceElement.getElementsByClassName('serviceName')[0].innerHTML = `<i class="${fav.icon}"></i> ${fav.name}`;
             serviceElement.getElementsByClassName('serviceName')[0].href = ser.link;
             servicesWrapper.appendChild(serviceElement);
         });
